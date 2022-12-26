e89e190a5c1764350d1c854a9aa1ae130a0ffcc1
KsanaSal <oasalivon@gmail.com>
2022-12-26 23:04:11 +0200

    Add styles in form and wrapper in photo

--- Screens/RegistrationScreen.js
+++ Screens/RegistrationScreen.js
@@ -1,27 +1,52 @@
-import { StyleSheet, View, Text, TextInput, Button } from "react-native";
+import {
+    StyleSheet,
+    View,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    Pressable,
+} from "react-native";
 
 export default function RegistrationScreen() {
     return (
         <View style={styles.container}>
-            <Text style={styles.text}>Регістрація</Text>
-            <View style={styles.form}>
-                <View>
-                    <Text style={styles.inputTitle}>EMAIL ADDRES</Text>
-                    <TextInput style={styles.input} textAlign={"center"} />
+            <View style={styles.wrap}>
+                <View style={styles.wrapPhoto}></View>
+                <View style={styles.icon}></View>
+            </View>
+
+            <View>
+                <Text style={styles.text}>Регістрація</Text>
+                <View style={styles.form}>
+                    <View>
+                        <TextInput
+                            style={styles.input}
+                            placeholder="Логін"
+                            placeholderTextColor="#BDBDBD"
+                        />
+                    </View>
+                    <View style={{ marginTop: 16 }}>
+                        <TextInput
+                            style={styles.input}
+                            placeholder="Адреса електронної почти"
+                            placeholderTextColor="#BDBDBD"
+                        />
+                    </View>
+                    <View style={{ marginTop: 16 }}>
+                        <TextInput
+                            style={styles.input}
+                            secureTextEntry={true}
+                            placeholder="Пароль"
+                            placeholderTextColor="#BDBDBD"
+                        />
+                    </View>
+                    <TouchableOpacity activeOpacity={0.8} style={styles.btn}>
+                        <Text style={styles.btnTitle}>Зареєструватися</Text>
+                    </TouchableOpacity>
                 </View>
-                <View>
-                    <Text style={styles.inputTitle}>EMAIL ADDRES</Text>
-                    <TextInput style={styles.input} textAlign={"center"} />
-                </View>
-                <View style={{ marginTop: 20 }}>
-                    <Text style={styles.inputTitle}>PASSWORD</Text>
-                    <TextInput
-                        style={styles.input}
-                        textAlign={"center"}
-                        secureTextEntry={true}
-                    />
-                </View>
-                <Button title="SIGN IN" />
+                <Pressable style={{ alignItems: "center" }}>
+                    <Text style={styles.linkText}>Вже є акаунт? Увійти</Text>
+                </Pressable>
             </View>
         </View>
     );
@@ -36,9 +61,38 @@ const styles = StyleSheet.create({
         borderTopRightRadius: 25,
         backgroundColor: "#ffffff",
         paddingHorizontal: 16,
+        // alignContent: "center",
+        // justifyContent: "center",
+    },
+
+    wrap: { marginBottom: 32 },
+
+    wrapPhoto: {
+        position: "absolute",
+        left: 128,
+        top: -60,
+        // alignItems: "center",
+        width: 120,
+        height: 120,
+        backgroundColor: "#F6F6F6",
+        borderRadius: 16,
+    },
+
+    icon: {
+        position: "relative",
+        bottom: -20,
+        left: 235,
+        color: "#FF6C00",
+        backgroundColor: "#fff",
+        borderWidth: 1,
+        borderColor: "#FF6C00",
+        borderRadius: 15,
+        width: 25,
+        height: 25,
     },
 
     text: {
+        marginTop: 32,
         marginBottom: 32,
         fontSize: 30,
         fontWeight: "medium",
@@ -49,23 +103,43 @@ const styles = StyleSheet.create({
     },
 
     form: {
-        marginHorizontal: 10,
-        zIndex: 1,
-    },
-
-    inputTitle: {
-        color: "red",
-        marginBottom: 10,
-        fontSize: 18,
+        marginBottom: 16,
+        // marginHorizontal: 16,
+        // zIndex: 1,
     },
 
     input: {
-        zIndex: 2,
+        padding: 16,
         borderWidth: 1,
-        borderColor: "red",
-        height: 40,
-        borderRadius: 6,
+        borderColor: "#E8E8E8",
+        height: 50,
+        borderRadius: 8,
+        backgroundColor: "#F6F6F6",
+        // placeholderTextColor: "red",
 
-        color: "#f0f8ff",
+        textAlign: "left",
+        fontSize: 16,
+        color: "#212121",
+    },
+
+    btn: {
+        backgroundColor: "#FF6C00",
+        height: 50,
+        borderRadius: 100,
+        marginTop: 40,
+        justifyContent: "center",
+        alignItems: "center",
+        // marginHorizontal: 20,
+    },
+
+    btnTitle: {
+        color: "#fff",
+        fontSize: 16,
+    },
+
+    linkText: {
+        fontSize: 16,
+        color: "#1B4371",
+        alignItems: "center",
     },
 });
