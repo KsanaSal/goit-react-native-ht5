4a0c5555727c5c78dff5efac3296d28cfd506012
KsanaSal <oasalivon@gmail.com>
2022-12-28 19:15:55 +0200

    Add font family

--- App.js
+++ App.js
@@ -1,10 +1,36 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View, ImageBackground, Dimensions } from "react-native";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
 import RegistrationScreen from "./Screens/RegistrationScreen";
+import { useEffect, useCallback } from "react";
 
 export default function App() {
+    const [fontsLoaded] = useFonts({
+        "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+        "Roboto-Italic": require("./assets/fonts/Roboto-Italic.ttf"),
+        "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
+    });
+
+    useEffect(() => {
+        async function prepare() {
+            await SplashScreen.preventAutoHideAsync();
+        }
+        prepare();
+    }, []);
+
+    const onLayout = useCallback(async () => {
+        if (fontsLoaded) {
+            await SplashScreen.hideAsync();
+        }
+    }, [fontsLoaded]);
+
+    if (!fontsLoaded) {
+        return null;
+    }
+
     return (
-        <View style={styles.container}>
+        <View style={styles.container} onLayout={onLayout}>
             <ImageBackground
                 source={require("./assets/images/photo-background-1x.jpg")}
                 style={styles.image}
@@ -19,6 +45,7 @@ export default function App() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        // fontFamily: "Roboto-Italic",
     },
     image: {
         position: "absolute",
