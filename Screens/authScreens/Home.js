186c8d4c6c364e205c35665ebdb18584db5c90bd
KsanaSal <oasalivon@gmail.com>
2023-01-04 20:14:19 +0200

    Added transition from PostsScreen to LoginScreen

--- Screens/authScreens/Home.js
+++ Screens/authScreens/Home.js
@@ -1,5 +1,5 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { Image } from "react-native";
+import { TouchableOpacity, Image } from "react-native";
 
 import PostsScreen from "../../Screens/mainScreens/PostsScreen";
 import CreatePostsScreen from "../../Screens/mainScreens/CreatePostsScreen";
@@ -7,16 +7,20 @@ import ProfileScreen from "../../Screens/mainScreens/ProfileScreen";
 
 const MainTab = createBottomTabNavigator();
 
-export default function Home() {
+export default function Home({ navigation }) {
     return (
         <MainTab.Navigator
             screenOptions={{
-                headerShown: false,
-                showLabel: false,
+                // headerShown: false,
+                // showLabel: false,
                 tabBarStyle: {
                     paddingBottom: 24,
                     paddingTop: 8,
                     height: 80,
+                    shadowColor: "rgba(0, 0, 0, 0.3)",
+                    shadowOffset: "0px -0.5px 0px",
+                },
+                headerStyle: {
                     shadowColor: "rgba(0, 0, 0, 0.3)",
                     shadowOffset: "0px -0.5px 0px",
                 },
@@ -26,6 +30,25 @@ export default function Home() {
                 name="Post"
                 component={PostsScreen}
                 options={{
+                    headerTitle: "Публікації",
+                    headerTitleStyle: {
+                        color: "#212121",
+                        fontSize: 18,
+                        fontFamily: "Roboto-Medium",
+                        fontWeight: "medium",
+                    },
+                    headerRight: () => {
+                        const img = require("../../assets/icon/icon-log-out.png");
+                        return (
+                            <TouchableOpacity
+                                onPress={() => navigation.navigate("Login")}
+                            >
+                                <Image source={img}></Image>
+                            </TouchableOpacity>
+                        );
+                    },
+                    headerTitleAlign: "center",
+                    headerRightContainerStyle: { paddingHorizontal: 16 },
                     tabBarShowLabel: false,
                     tabBarIcon: () => {
                         const img = require("../../assets/icon/icon-grid.png");
@@ -48,6 +71,7 @@ export default function Home() {
                 name="Profile"
                 component={ProfileScreen}
                 options={{
+                    headerShown: false,
                     tabBarShowLabel: false,
                     tabBarIcon: () => {
                         const img = require("../../assets/icon/icon-user.png");
--- Screens/mainScreens/PostsScreen.js
+++ Screens/mainScreens/PostsScreen.js
@@ -1,8 +1,15 @@
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Image, Dimensions } from "react-native";
 
 export default function PostsScreen() {
     return (
         <View style={styles.container}>
+            {/* <View style={styles.header}>
+                <Text style={styles.text}>Публікації</Text>
+                <Image
+                    source={require("../../assets/icon/icon-log-out.png")}
+                ></Image>
+            </View> */}
+            
             <Text>Публікація</Text>
         </View>
     );
@@ -11,8 +18,42 @@ export default function PostsScreen() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: "center",
+        // justifyContent: "center",
+        alignContent: "center",
         alignItems: "center",
         backgroundColor: "#ffffff",
+        paddingHorizontal: 16,
+    },
+
+    header: {
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "flex-end",
+        height: 88,
+        width: Dimensions.get("window").width,
+        paddingTop: 55,
+        paddingHorizontal: 16,
+        paddingBottom: 10,
+        borderWidth: 1,
+        // paddingHorizontal: 16,
+        shadowColor: "rgba(0, 0, 0, 0.3)",
+        shadowOffset: "0px 0.5px 0px",
+        backgroundColor: "#fff",
+        // flex: 1,
+        // justifyContent: "center",
+        // backgroundColor: "#ffffff",
+    },
+
+    text: {
+        // marginTop: 55,
+        marginRight: 130,
+        fontFamily: "Roboto-Medium",
+        fontWeight: "medium",
+        fontSize: 17,
+        lineHeight: 22,
+        letterSpacing: -0.41,
+        textAlign: "center",
+        color: "#212121",
     },
 });
